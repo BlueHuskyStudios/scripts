274f6d2d432ba7944bb7b2e8b15809913969cb35
Ben Leggiero <BenLeggiero@Gmail.com>
2020-12-27 00:38:22 -0700

    Element replacer buttons now react to being pressed

--- assets/v2/general/kt/src/org/bh/scripts/pageMutation/ContentReplacers.kt
+++ assets/v2/general/kt/src/org/bh/scripts/pageMutation/ContentReplacers.kt
@@ -2,6 +2,7 @@ package org.bh.scripts.pageMutation
 
 import jQueryInterface.jq
 import org.bh.scripts.general.utilities.isCapitalized
+import org.w3c.dom.Element
 import kotlin.js.Json
 
 
@@ -17,26 +18,64 @@ object ContentReplacers {
 
     private fun runContentReplacers() {
         contentReplacers.forEach {
-            it.run()
+            it.connectAndRun()
         }
     }
 }
 
 
 
-private fun ContentReplacerMapElement.run() {
-    val contentReplacerSelectorUi = jq("[data-replaced-element-selector=\"${selector}\"]")
+private fun ContentReplacerMapElement.connectAndRun() {
+    val contentReplacerSelectorUi = jq("[data-replaced-element-selector=\"${selectorForAllElementsWithContentToBeReplaced}\"]")
     if (contentReplacerSelectorUi.length < 1) { return }
 
     val contentReplacerDataSourceDataKey = contentReplacerSelectorUi.data("content-replacer-data-source") as? String ?: return
     val contentReplacerDataReplacementDataKey = contentReplacerSelectorUi.data("content-replacer-data-replacement") as? String ?: return
+    val contentReplacerDataReplacementSelector = "[$contentReplacerDataReplacementDataKey]"
 
-    val userSelectedContentReplacement = jq("[data-pronoun-gender][selected]")
+    connectButtons(contentReplacerDataSourceDataKey= contentReplacerDataSourceDataKey,
+        contentReplacerDataReplacementSelector= contentReplacerDataReplacementSelector,
+        contentReplacerDataReplacementDataKey= contentReplacerDataReplacementDataKey)
+
+    run(contentReplacerDataSourceDataKey= contentReplacerDataSourceDataKey,
+        contentReplacerDataReplacementSelector= contentReplacerDataReplacementSelector,
+        contentReplacerDataReplacementDataKey= contentReplacerDataReplacementDataKey)
+}
+
+
+private fun ContentReplacerMapElement.connectButtons(contentReplacerDataSourceDataKey: String,
+                                                     contentReplacerDataReplacementSelector: String,
+                                                     contentReplacerDataReplacementDataKey: String) {
+    jq(contentReplacerDataReplacementSelector).click {
+        val currentTarget = it.currentTarget as? Element ?: return@click
+        clearSelection(contentReplacerDataReplacementSelector= contentReplacerDataReplacementSelector)
+        select(currentTarget)
+        run(contentReplacerDataSourceDataKey= contentReplacerDataSourceDataKey,
+            contentReplacerDataReplacementSelector= contentReplacerDataReplacementSelector,
+            contentReplacerDataReplacementDataKey= contentReplacerDataReplacementDataKey)
+    }
+}
+
+
+private fun ContentReplacerMapElement.clearSelection(contentReplacerDataReplacementSelector: String) {
+    jq(contentReplacerDataReplacementSelector).attr("selected", null as String?)
+}
+
+
+private fun ContentReplacerMapElement.select(element: Element) {
+    jq(element).select()
+}
+
+
+private fun ContentReplacerMapElement.run(contentReplacerDataSourceDataKey: String,
+                                          contentReplacerDataReplacementSelector: String,
+                                          contentReplacerDataReplacementDataKey: String) {
+    val userSelectedContentReplacement = jq("$contentReplacerDataReplacementSelector[selected]")
     if (userSelectedContentReplacement.length != 1) { return }
 
     val userSelectedDataReplacement = userSelectedContentReplacement.data(contentReplacerDataReplacementDataKey) as? String ?: return
 
-    val elementsWithContentToBeReplaced = jq(selector)
+    val elementsWithContentToBeReplaced = jq(selectorForAllElementsWithContentToBeReplaced)
     elementsWithContentToBeReplaced.each { _, element ->
         val contentReplacerDataSource = jq(element).data(contentReplacerDataSourceDataKey) as? String ?: return@each
         val result = replacer(contentReplacerDataSource, userSelectedDataReplacement)
@@ -75,7 +114,7 @@ private fun ContentReplacerMapElement.run() {
 }
 
 
-private inline val ContentReplacerMapElement.selector get() = key
+private inline val ContentReplacerMapElement.selectorForAllElementsWithContentToBeReplaced get() = key
 private inline val ContentReplacerMapElement.replacer get() = value
 
 
